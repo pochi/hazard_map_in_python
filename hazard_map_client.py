39e1973ac097553b56f36b2b8166c446dbffb4a3
pochi <pochi.black@gmail.com>
2023-05-05 16:41:42 +0900

    Add multiprocess and multithread

--- hazard_map_client.py
+++ hazard_map_client.py
@@ -5,8 +5,10 @@ from xml.dom import minidom
 from tqdm import tqdm
 import requests
 from multiprocessing import Pool, RLock
+from concurrent.futures import ThreadPoolExecutor
 import numpy as np
 import pathlib
+from functools import partial
 
 def _parse(xml_file):
     title_and_identifier_and_tile = []
@@ -41,28 +43,86 @@ def _make_urls(tile):
 
     return urls
 
-def download_images(process_num, urls, output_dir):
+
+def download_image(prefix, output_base_dir, cache):
     # title_and_identifier_and_tile = _parse(xml_file)
     base_url = 'https://disaportaldata.gsi.go.jp/raster/01_flood_l2_shinsuishin_kuni_data/'
+    url = base_url + prefix + ".png"
+    output_dir = output_base_dir + prefix
+    # print(url)
+    output_not_found_file = os.path.join(output_dir, "404.txt")
+    output_file = os.path.join(output_dir, "200.png")
 
+    if os.path.exists(output_not_found_file) and cache == 0:
+        return True
+
+    if os.path.exists(output_file) and cache == 0:
+        return True
+
+    response = requests.get(url)
+    if response.status_code == 404:
+        os.makedirs(output_dir)
+        pathlib.Path(output_not_found_file).touch()
+        return True
+
+    if response.status_code != 200:
+        raise Exception("HTTP status " + str(response.status_code) + ": " + url)
+
+    print(url)
+    if os.path.exists(output_not_found_file):
+        os.remove(output_not_found_file)
+
+    os.makedirs(output_dir)
+    with open(output_file, "wb") as f:
+        f.write(response.content)
+
+    return True
+
+
+"""
     for prefix in tqdm(urls, desc=f'#{process_num:>2}', position=process_num+1):
         url = base_url + prefix
+        output_dir = output_base_dir + prefix
+        output_not_found_file = os.path.join(output_dir, "404.txt")
+        output_file = os.path.join(output_dir, "200.png")
+
+        if os.path.exists(output_not_found_file) and cache == 0:
+            continue
+
+        if os.path.exists(output_file) and cache == 0:
+            continue
+
         response = requests.get(url)
         if response.status_code == 404:
-            output_dir = output_dir + prefix
             os.makedirs(output_dir)
-            pathlib.Path(os.path.join(output_dir, "404.txt")).touch()
+            pathlib.Path(output_not_found_file).touch()
             continue
-            
 
         if response.status_code != 200:
             raise Exception("HTTP status " + str(response.status_code) + ": " + url)
 
         print(url)
-        image = response.content
+        if os.path.exists(output_not_found_file):
+            os.remove(output_not_found_file)
 
-    return process_num
+        os.makedirs(output_dir)
+        with open(output_file, "wb") as f:
+            f.write(response.content)
 
+    return True
+"""
+
+def download_images(process_num, urls, output_base_dir, cache):
+    partial_download_image = partial(download_image, output_base_dir=output_base_dir, cache=cache)
+    with ThreadPoolExecutor(max_workers=2) as e:
+        results = list(tqdm(
+            # e.starmap(download_image, [(prefix, output_base_dir, cache) for prefix in urls]), 
+            e.map(partial_download_image, urls), 
+            desc=f'#{process_num:>2}',
+            total=len(urls), 
+            position=process_num+1
+        ))
+    return results
 
 def fetch_hazard_map_images(config):
     xml_file = config["job"]["metadata_file"]
@@ -71,7 +131,7 @@ def fetch_hazard_map_images(config):
     urls = _make_urls(config["job"]["tile"])
     process_num = os.cpu_count() - 1
     divide_urls = np.array_split(urls, process_num)
-    divide_urls_with_process_nums = [(n, divide_urls[n], config["output"]["dir"]) for n in range(process_num)]
+    divide_urls_with_process_nums = [(n, divide_urls[n], config["output"]["dir"], config["job"]["cache"]) for n in range(process_num)]
     with Pool(
         process_num,
         initializer=tqdm.set_lock,
